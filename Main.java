04070483d9283db0423e6a5732b17fd0639c883c
ninaahuangg <thenina207@gmail.com>
2024-09-11 03:46:53 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main
+{
+   public static void main(String[] args)
+    {
+    final double p = 1.2;
+    double a = 5;
+    a = a + 1;
+    double b;
+    b = (p * a);
+    boolean t = true;
+    String s = "The number above is greater than 1.";
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(t);
+    int i = -5;
+    int u = 4;
+    int q;
+    q = (i + u);
+    String k = "The number above is greater than 1.";
+    boolean f = false;
+        System.out.println(" ");
+        System.out.println(q);
+        System.out.println(k);
+        System.out.println(f);
+    }
+}
